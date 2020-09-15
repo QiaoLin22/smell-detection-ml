9ae64715007cc3e3584a0ebd99ebd9f6a8fc3fbe
Qiao Lin <qiaolin@andrew.cmu.edu>
2020-09-15 01:32:56 -0700

    add ui support

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ from algo import compute
 import json
 import glob
 import os
-pp = pprint.PrettyPrinter(indent=4)
+import tkinter
+from tkinter import messagebox
 
+root = tkinter.Tk()
+root.withdraw()
+pp = pprint.PrettyPrinter(indent=4)
 def train():
     envyres = compute("feature-envy.arff")
     datares = compute("data-class.arff")
@@ -27,7 +31,7 @@ else:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('operation',type = str,help='Choose your operation(listAll, modelAcc, compare)')
+    parser.add_argument('operation',type = str,help='Choose your operation(listAll, modelAcc, compare,retrain)')
     parser.add_argument('--model',type=str, help='Choose the model used for the Training set(decision_tree, random_forest, naive_bayes, svc_linear, svc_poly, svc_rbf, svc_sigmoid)')
     parser.add_argument('--smell','--list', nargs='+', help='Choose the smells(feature-envy, data-class, god-class, long-method), could be multiple smells for modelAcc, only one for compare')
     args, unknown = parser.parse_known_args()
@@ -36,6 +40,11 @@ def main():
 def control(args,database):
     smelllist = ['feature-envy','data-class','god-class','long-method']
     modellist = ['decision_tree','random_forest','naive_bayes','svc_linear','svc_poly','svc_rbf','svc_sigmoid']
+    if args.operation == 'help':
+        messagebox.showinfo("Information", "Welcome! \n Supported operations(listAll, run, compare,retrain). \r\n Supported models(decision_tree, random_forest, naive_bayes, svc_linear, svc_poly, svc_rbf, svc_sigmoid)."
+                                           "\r\n Supported smells(feature-envy, data-class, god-class, long-method), smell input could be multiple for modelAcc, only one for compare. \r\n listAll list all available trained result,"
+                                           "retrain discard previous training result and retrain. \r\n listAll and retrain don't take other iput. Run as 'python main.py listAll'. \r\n run and compare take two other inputs, model and smell,"
+                                           "run take one model and multiple smell, compare take one model and only one smell.\r\n Run as ' python main.py compare --model decision_tree --smell feature-envy' or ' python main.py run --model naive_bayes --smell long-method god-class'.")
     if args.operation == 'retrain':
         if os.path.exists("database.json"):
             os.remove("database.json")
@@ -49,22 +58,22 @@ def control(args,database):
         list = args.smell
         for db in list:
             if db not in smelllist:
-                warnings.warn('your smell input contains non-existing smell')
+                messagebox.showwarning("Warning", 'your smell input contains non-existing smell')
         else:
             if args.model not in modellist:
-                warnings.warn('Input model not valid...')
+                messagebox.showwarning("Warning", 'Input model not valid...')
             else:
                 modelAcc(args.model,args.smell,database)
     if args.operation == 'compare':
         list = args.smell
         if(len(list) > 1):
-            warnings.warn("Compare can only have 1 smell input")
+            messagebox.showwarning("Warning", "Compare can only have 1 smell input")
         else:
             smell = list[0]
             if args.model not in modellist:
-                warnings.warn('Input model not valid...')
+                messagebox.showwarning("Warning",'Input model not valid...')
             if smell not in smelllist:
-                warnings.warn('there is no file associated with this smell')
+                messagebox.showwarning("Warning", 'there is no file associated with this smell')
             else:
                 compare(args.model, args.smell,database)
 
